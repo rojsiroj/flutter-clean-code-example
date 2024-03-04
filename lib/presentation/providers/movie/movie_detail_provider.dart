62d5487e60a1aa6fe5d7196b7a057626b4912bcc
rojsiroj <sirojudin.dev@gmail.com>
2024-03-04 21:29:11 +0700

    Movie Detail Provider & Actors Provider

---
+++ lib/presentation/providers/movie/movie_detail_provider.dart
@@ -0,0 +1,23 @@
+import 'package:flix_id/domain/entities/movie.dart';
+import 'package:flix_id/domain/entities/movie_detail.dart';
+import 'package:flix_id/domain/entities/result.dart';
+import 'package:flix_id/domain/usecases/get_movie_detail/get_movie_detail.dart';
+import 'package:flix_id/domain/usecases/get_movie_detail/get_movie_detail_params.dart';
+import 'package:flix_id/presentation/providers/usecases/get_movie_detail_provider.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'movie_detail_provider.g.dart';
+
+@riverpod
+Future<MovieDetail?> movieDetail(MovieDetailRef ref,
+    {required Movie movie}) async {
+  GetMovieDetail getMovieDetail = ref.read(getMovieDetailProvider);
+
+  var movieDetailResult =
+      await getMovieDetail(GetMovieDetailParams(movie: movie));
+
+  return switch (movieDetailResult) {
+    Success(value: final movieDetail) => movieDetail,
+    Failed(message: _) => null
+  };
+}
