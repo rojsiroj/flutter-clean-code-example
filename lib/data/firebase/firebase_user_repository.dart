e30fc91eaeb6a160023ccf8a8d7b6f532a6f2b07
rojsiroj <sirojudin.dev@gmail.com>
2024-03-04 10:54:15 +0700

    handle firebase user repository

--- lib/data/firebase/firebase_user_repository.dart
+++ lib/data/firebase/firebase_user_repository.dart
@@ -1,10 +1,13 @@
 import 'dart:io';
 
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flix_id/data/repositories/user_repository.dart';
 import 'package:flix_id/domain/entities/result.dart';
 import 'package:flix_id/domain/entities/user.dart';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:path/path.dart';
 
 class FirebaseUserRepository implements UserRepository {
   final FirebaseFirestore _firebaseFireStore;
@@ -18,9 +21,25 @@ class FirebaseUserRepository implements UserRepository {
       required String email,
       required String name,
       String? photoUrl,
-      int balance = 0}) {
-    // TODO: implement createUser
-    throw UnimplementedError();
+      int balance = 0}) async {
+    CollectionReference<Map<String, dynamic>> users =
+        _firebaseFireStore.collection('users');
+
+    await users.doc(uid).set({
+      'uid': uid,
+      'email': email,
+      'name': name,
+      'photoUrl': photoUrl,
+      'balance': balance,
+    });
+
+    DocumentSnapshot<Map<String, dynamic>> result = await users.doc(uid).get();
+
+    if (result.exists) {
+      return Result.success(User.fromJson(result.data()!));
+    } else {
+      return const Result.failed('Failed to create user');
+    }
   }
 
   @override
@@ -39,28 +58,95 @@ class FirebaseUserRepository implements UserRepository {
   }
 
   @override
-  Future<Result<int>> getUserBalance({required String uid}) {
-    // TODO: implement getUserBalance
-    throw UnimplementedError();
+  Future<Result<int>> getUserBalance({required String uid}) async {
+    DocumentReference<Map<String, dynamic>> documentReference =
+        _firebaseFireStore.doc('users/$uid');
+    DocumentSnapshot<Map<String, dynamic>> result =
+        await documentReference.get();
+
+    if (result.exists) {
+      return Result.success(result.data()!['balance']);
+    } else {
+      return const Result.failed('User not found');
+    }
   }
 
   @override
-  Future<Result<User>> updateUser({required User user}) {
-    // TODO: implement updateUser
-    throw UnimplementedError();
+  Future<Result<User>> updateUser({required User user}) async {
+    try {
+      DocumentReference<Map<String, dynamic>> documentReference =
+          _firebaseFireStore.doc('users/${user.uid}');
+      await documentReference.update(user.toJson());
+
+      DocumentSnapshot<Map<String, dynamic>> result =
+          await documentReference.get();
+
+      if (result.exists) {
+        User updatedUser = User.fromJson(result.data()!);
+        if (updatedUser == user) {
+          return Result.success(updatedUser);
+        } else {
+          return const Result.failed('Failed to update user data');
+        }
+      } else {
+        return const Result.failed('Failed to update user data');
+      }
+    } on FirebaseException catch (e) {
+      return Result.failed(e.message ?? 'Failed to update user data');
+    }
   }
 
   @override
   Future<Result<User>> updateUserBalance(
-      {required String uid, required int balance}) {
-    // TODO: implement updateUserBalance
-    throw UnimplementedError();
+      {required String uid, required int balance}) async {
+    DocumentReference<Map<String, dynamic>> documentReference =
+        _firebaseFireStore.doc('users/$uid');
+    DocumentSnapshot<Map<String, dynamic>> result =
+        await documentReference.get();
+
+    if (result.exists) {
+      await documentReference.update({'balance': balance});
+
+      DocumentSnapshot<Map<String, dynamic>> updatedResult =
+          await documentReference.get();
+
+      if (updatedResult.exists) {
+        User updatedUser = User.fromJson(updatedResult.data()!);
+        if (updatedUser.balance == balance) {
+          return Result.success(updatedUser);
+        } else {
+          return const Result.failed('Failed to update user balance');
+        }
+      } else {
+        return const Result.failed('Failed to retrieve updated user balance');
+      }
+    } else {
+      return const Result.failed('User not found');
+    }
   }
 
   @override
   Future<Result<User>> uploadProfilePicture(
-      {required User user, required File imageFile}) {
-    // TODO: implement uploadProfilePicture
-    throw UnimplementedError();
+      {required User user, required File imageFile}) async {
+    String filename = basename(imageFile.path);
+
+    Reference reference = FirebaseStorage.instance.ref().child(filename);
+
+    try {
+      await reference.putFile(imageFile);
+
+      String downloadUrl = await reference.getDownloadURL();
+
+      var updateResult =
+          await updateUser(user: user.copyWith(photoUrl: downloadUrl));
+
+      if (updateResult.isSuccess) {
+        return Result.success(updateResult.resultValue!);
+      } else {
+        return Result.failed(updateResult.errorMessage!);
+      }
+    } catch (e) {
+      return const Result.failed('Failed to upload profile picture');
+    }
   }
 }
