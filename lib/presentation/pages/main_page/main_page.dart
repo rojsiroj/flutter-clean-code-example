a61a2a92a46865176b0347ed3808b6fd58247f84
rojsiroj <sirojudin.dev@gmail.com>
2024-03-07 11:25:31 +0700

    initiate bottom nav bar

--- lib/presentation/pages/main_page/main_page.dart
+++ lib/presentation/pages/main_page/main_page.dart
@@ -1,6 +1,9 @@
 import 'package:flix_id/presentation/extensions/build_context_extension.dart';
+import 'package:flix_id/presentation/misc/methods.dart';
 import 'package:flix_id/presentation/providers/router/router_provider.dart';
 import 'package:flix_id/presentation/providers/user_data/user_data_provider.dart';
+import 'package:flix_id/presentation/widgets/bottom_nav_bar.dart';
+import 'package:flix_id/presentation/widgets/bottom_vav_bar_item.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -25,20 +28,33 @@ class _MainPageState extends ConsumerState<MainPage> {
       appBar: AppBar(
         title: const Text('Main Page'),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            Text(ref.watch(userDataProvider).when(
-                data: (data) => data.toString(),
-                error: (error, stackTrace) => '',
-                loading: () => 'Loading')),
-            ElevatedButton(
-                onPressed: () {
-                  ref.read(userDataProvider.notifier).logout();
-                },
-                child: const Text('Logout'))
-          ],
-        ),
+      body: Stack(
+        children: [
+          Center(
+            child: Column(
+              children: [
+                Text(ref.watch(userDataProvider).when(
+                    data: (data) => data.toString(),
+                    error: (error, stackTrace) => '',
+                    loading: () => 'Loading')),
+                ElevatedButton(
+                    onPressed: () {
+                      ref.read(userDataProvider.notifier).logout();
+                    },
+                    child: const Text('Logout')),
+                verticalSpace(50),
+              ],
+            ),
+          ),
+          BottomNavBar(items: [
+            BottomNavBarItem(
+                index: 0,
+                isSelected: false,
+                title: 'Home',
+                image: 'assets/movie.png',
+                selectedImage: 'assets/movie-selected.png')
+          ], onTap: (index) {}, selectedIndex: 0)
+        ],
       ),
     );
   }
